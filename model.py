2fcf07586af6c035a999ebfa51e654c11da4fed6
syahrulhamdani <hamdani.inbox@gmail.com>
2018-10-05 00:19:11 +0700

    delete TODO

--- model.py
+++ model.py
@@ -13,10 +13,3 @@ class Model():
     """Model wrapper"""
     def __init__(self):
         pass
-
-# TODO:
-# 1. function load_pretrained with classifier and pretrained model name
-# 2. function deep_learning to train the model
-# 3. function validate_model to validate the model used in deep_learning
-#    function
-# 4. function test_model to test the model after being trained
